1c186e8761ce82a46eef7d1294a17958bcd98954
Lars Wikman <lars@underjord.io>
2022-06-03 15:54:38 +0200

    Implement smoother chapter adding API

--- lib/id3vx/tag.ex
+++ lib/id3vx/tag.ex
@@ -29,6 +29,23 @@ defmodule Id3vx.Tag do
     %Tag{version: version, revision: 0, frames: []}
   end
 
+  def add_attached_picture(container, description, mime_type, image_data, picture_type \\ :other) do
+    frame = %Frame{
+      id: "APIC",
+      flags: FrameFlags.all_false(),
+      label: Id3vx.Frame.Labels.from_id("APIC"),
+      data: %Frame.AttachedPicture{
+        encoding: :utf16,
+        mime_type: mime_type,
+        picture_type: picture_type,
+        description: description,
+        image_data: image_data
+      }
+    }
+
+    %{container | frames: [frame | container.frames]}
+  end
+
   def add_text_frame(container, frame_id, text) when is_binary(text) do
     add_text_frame(container, frame_id, [text])
   end
@@ -45,5 +62,162 @@ defmodule Id3vx.Tag do
     }
 
     %{container | frames: [frame | container.frames]}
+  end
+
+  @typical_element_prefix "chp"
+  def add_typical_chapter_and_toc(
+        tag,
+        start_time,
+        end_time,
+        start_offset,
+        end_offset,
+        title,
+        callback
+      )
+      when is_function(callback) do
+    {toc_okay?, toc_frame} =
+      case find_element(tag, "CTOC", "toc") do
+        nil ->
+          {true, nil}
+
+        %Frame{data: toc} = f ->
+          {toc.ordered and
+             toc.top_level and
+             Enum.all?(toc.child_elements, fn eid ->
+               String.starts_with?(eid, @typical_element_prefix)
+             end), f}
+      end
+
+    # Ensure we build a ToC
+    {tag, toc_frame} =
+      case toc_frame do
+        nil ->
+          toc_frame = %Frame{
+            id: "CTOC",
+            flags: FrameFlags.all_false(),
+            label: Id3vx.Frame.Labels.from_id("CTOC"),
+            data: %Frame.TableOfContents{
+              element_id: "toc",
+              top_level: true,
+              ordered: true,
+              child_elements: []
+            }
+          }
+
+          {%{tag | frames: [toc_frame | tag.frames]}, toc_frame}
+
+        toc_frame ->
+          {tag, toc_frame}
+      end
+
+    if toc_okay? do
+      num =
+        case List.last(toc_frame.data.child_elements) do
+          nil ->
+            0
+
+          <<"chp", num::binary>> ->
+            String.to_integer(num)
+        end
+
+      chapter_element_id = @typical_element_prefix <> to_string(num + 1)
+
+      chapter =
+        %Frame.Chapter{
+          element_id: chapter_element_id,
+          start_time: start_time,
+          end_time: end_time,
+          start_offset: start_offset,
+          end_offset: end_offset,
+          frames: []
+        }
+        |> add_text_frame("TIT2", title)
+        |> callback.()
+
+      chapter_frame = %Frame{
+        id: "CHAP",
+        flags: FrameFlags.all_false(),
+        label: Id3vx.Frame.Labels.from_id("CHAP"),
+        data: chapter
+      }
+
+      # Append the child element in the ToC child elements
+      toc_frame = %{
+        toc_frame
+        | data: %{
+            toc_frame.data
+            | child_elements: toc_frame.data.child_elements ++ [chapter_element_id]
+          }
+      }
+
+      %{tag | frames: tag.frames ++ [chapter_frame]}
+      |> change_element("CTOC", "toc", fn _ ->
+        toc_frame.data
+      end)
+    else
+      raise Id3vx.Error, message: "Not a typical CTOC frame, can't modify it typically"
+    end
+  end
+
+  @spec change_elements(container :: map(), frame_id :: String.t(), callback :: fun()) :: map()
+  def change_elements(container, frame_id, callback) do
+    frames =
+      Enum.map(container.frames, fn frame ->
+        if frame.id == frame_id do
+          callback.(frame)
+        else
+          frame
+        end
+      end)
+
+    %{container | frames: frames}
+  end
+
+  @spec change_element(
+          container :: map(),
+          frame_id :: String.t(),
+          element_id :: String.t(),
+          callback :: fun()
+        ) :: map()
+  def change_element(container, frame_id, element_id, callback) do
+    frames =
+      Enum.map(container.frames, fn frame ->
+        case frame do
+          %Frame{id: ^frame_id, data: %Frame.Chapter{element_id: ^element_id}} ->
+            %{frame | data: callback.(frame.data)}
+
+          %Frame{id: ^frame_id, data: %Frame.TableOfContents{element_id: ^element_id}} ->
+            %{frame | data: callback.(frame.data)}
+
+          _ ->
+            frame
+        end
+      end)
+
+    %{container | frames: frames}
+  end
+
+  @spec find_element(container :: map(), frame_id :: String.t()) :: Frame.t() | nil
+  def find_element(container, frame_id) do
+    Enum.find(container.frames, fn f ->
+      f.id == frame_id
+    end)
+  end
+
+  @spec find_element(container :: map(), frame_id :: String.t(), element_id :: String.t()) ::
+          Frame.t() | nil
+  def find_element(container, frame_id, element_id) do
+    Enum.find(container.frames, fn frame ->
+      case frame do
+        %Frame{id: ^frame_id, data: %Frame.Chapter{element_id: ^element_id}} ->
+          true
+
+        %Frame{id: ^frame_id, data: %Frame.TableOfContents{element_id: ^element_id}} ->
+          true
+
+        _ ->
+          false
+      end
+    end)
   end
 end
--- test/id3vx/tag_building_test.exs
+++ test/id3vx/tag_building_test.exs
@@ -1,7 +1,10 @@
 defmodule Id3vx.TagBuilderTest do
   use ExUnit.Case
 
+  alias Id3vx.Frame
   alias Id3vx.Tag
+  alias Id3vx.Frame.Chapter
+  alias Id3vx.Frame.TableOfContents
 
   test "build basic tag v2.3 with some text frames" do
     tag =
@@ -22,5 +25,189 @@ defmodule Id3vx.TagBuilderTest do
     |> Enum.each(fn {f1, f2} ->
       assert [f1.data.text] == f2.data.text
     end)
+  end
+
+  test "build basic tag v2.3 with some chapter frames" do
+    tag =
+      Tag.create(3)
+      |> Tag.add_typical_chapter_and_toc(
+        0,
+        20,
+        0,
+        1024,
+        "Chapter 1",
+        fn chapter ->
+          chapter
+          |> Tag.add_text_frame("TPUB", "Chapter publisher")
+          # Not a real picture
+          |> Tag.add_attached_picture("", "image/jpeg", <<255, 0, 255>>)
+        end
+      )
+      |> Tag.add_typical_chapter_and_toc(
+        21,
+        40,
+        1025,
+        2048,
+        "Chapter 2",
+        fn chapter ->
+          chapter
+          |> Tag.add_text_frame("TPUB", "Chapter publisher")
+          |> Tag.add_attached_picture("", "image/jpeg", <<255, 0, 255>>)
+        end
+      )
+      |> Tag.add_typical_chapter_and_toc(
+        41,
+        60,
+        2049,
+        3048,
+        "Chapter 3",
+        fn chapter ->
+          chapter
+          |> Tag.add_text_frame("TPUB", "Chapter publisher")
+          |> Tag.add_attached_picture("", "image/jpeg", <<255, 0, 255>>)
+        end
+      )
+
+    assert %Frame{
+             id: "CTOC",
+             data: %TableOfContents{
+               element_id: "toc",
+               child_elements: ["chp1", "chp2", "chp3"]
+             }
+           } = Tag.find_element(tag, "CTOC", "toc")
+
+    # Check that it encodes and re-parses
+    # TODO: Currently missing APIC encoding
+    # binary = Id3vx.encode_tag(tag)
+    # tag = Id3vx.parse_binary!(binary)
+
+    assert %Tag{
+             frames: [
+               %Id3vx.Frame{
+                 data: %Id3vx.Frame.TableOfContents{
+                   child_elements: ["chp1", "chp2", "chp3"],
+                   element_id: "toc",
+                   frames: [],
+                   ordered: true,
+                   top_level: true
+                 },
+                 flags: %Id3vx.FrameFlags{
+                   compression: false,
+                   data_length_indicator: false,
+                   encryption: false,
+                   file_alter_preservation: false,
+                   grouping_identity: false,
+                   read_only: false,
+                   tag_alter_preservation: false,
+                   unsynchronisation: false
+                 },
+                 id: "CTOC"
+               },
+               %Id3vx.Frame{
+                 data: %Id3vx.Frame.Chapter{
+                   element_id: "chp1",
+                   end_offset: 1024,
+                   end_time: 20,
+                   frames: [
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.AttachedPicture{
+                         description: "",
+                         encoding: :utf16,
+                         image_data: <<255, 0, 255>>,
+                         mime_type: "image/jpeg",
+                         picture_type: :other
+                       },
+                       id: "APIC",
+                       label: "Attached picture"
+                     },
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.Text{
+                         encoding: :utf16,
+                         text: ["Chapter publisher"]
+                       },
+                       id: "TPUB",
+                       label: "Publisher",
+                       size: nil
+                     },
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.Text{encoding: :utf16, text: ["Chapter 1"]},
+                       id: "TIT2"
+                     }
+                   ],
+                   start_offset: 0,
+                   start_time: 0
+                 },
+                 id: "CHAP"
+               },
+               %Id3vx.Frame{
+                 data: %Id3vx.Frame.Chapter{
+                   element_id: "chp2",
+                   end_offset: 2048,
+                   end_time: 40,
+                   frames: [
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.AttachedPicture{
+                         description: "",
+                         encoding: :utf16,
+                         image_data: <<255, 0, 255>>,
+                         mime_type: "image/jpeg",
+                         picture_type: :other
+                       },
+                       id: "APIC"
+                     },
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.Text{
+                         encoding: :utf16,
+                         text: ["Chapter publisher"]
+                       },
+                       id: "TPUB"
+                     },
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.Text{encoding: :utf16, text: ["Chapter 2"]},
+                       id: "TIT2"
+                     }
+                   ],
+                   start_offset: 1025,
+                   start_time: 21
+                 },
+                 id: "CHAP"
+               },
+               %Id3vx.Frame{
+                 data: %Id3vx.Frame.Chapter{
+                   element_id: "chp3",
+                   end_offset: 3048,
+                   end_time: 60,
+                   frames: [
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.AttachedPicture{
+                         description: "",
+                         encoding: :utf16,
+                         image_data: <<255, 0, 255>>,
+                         mime_type: "image/jpeg",
+                         picture_type: :other
+                       },
+                       id: "APIC"
+                     },
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.Text{
+                         encoding: :utf16,
+                         text: ["Chapter publisher"]
+                       },
+                       id: "TPUB"
+                     },
+                     %Id3vx.Frame{
+                       data: %Id3vx.Frame.Text{encoding: :utf16, text: ["Chapter 3"]},
+                       id: "TIT2"
+                     }
+                   ],
+                   start_offset: 2049,
+                   start_time: 41
+                 },
+                 id: "CHAP"
+               }
+             ],
+             revision: 0,
+             version: 3
+           } = tag
   end
 end
