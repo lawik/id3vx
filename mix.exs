8c57e57369397e9520656b6610212dcbe7d54eaa
Lars Wikman <lars@underjord.io>
2022-06-05 14:07:44 +0200

    Add deps for feed test

--- mix.exs
+++ mix.exs
@@ -13,16 +13,22 @@ defmodule Id3vx.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    if Mix.env() in [:dev, :test] do
+      [
+        extra_applications: [:logger, :xmerl]
+      ]
+    else
+      [
+        extra_applications: [:logger]
+      ]
+    end
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:feeder_ex, only: [:dev, :test]},
+      {:req, "~> 0.2.2", only: [:dev, :test]}
     ]
   end
 end
