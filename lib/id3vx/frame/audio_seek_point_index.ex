3f43bb888282f725d329ef6048ac1ae201044f23
Maqbool <maqbool@maqbool.net>
2022-08-29 10:18:31 +0530

    update

--- lib/id3vx/frame/audio_seek_point_index.ex
+++ lib/id3vx/frame/audio_seek_point_index.ex
@@ -1,15 +1,20 @@
 defmodule Id3vx.Frame.AudioSeekPointIndex do
+  @moduledoc """
+    ASPI frame struct
+  """
   defstruct indexed_data_start: nil,
             indexed_data_length: nil,
             number_of_index_points: nil,
-            bits_per_index_point: nil
+            bits_per_index_point: nil,
+            fraction_at_index: nil
 
   alias Id3vx.Frame.AudioSeekPointIndex
 
   @type t :: %AudioSeekPointIndex{
-          indexed_data_start: binary(),
-          indexed_data_length: binary(),
-          number_of_index_points: binary(),
-          bits_per_index_point: binary()
+          indexed_data_start: integer(),
+          indexed_data_length: integer(),
+          number_of_index_points: integer(),
+          bits_per_index_point: integer(),
+          fraction_at_index: binary()
         }
 end
